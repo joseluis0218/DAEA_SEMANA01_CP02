7199c0401d1fb877be9f1c95e8515661b0d919a5
Jose Luis Cristobal <jose.cristobal@techie.pe>
2019-08-12 23:51:31 -0500

    Agregar archivos de proyecto.

---
+++ Semana01_CP02/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Semana01_CP02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboClientes = new System.Windows.Forms.ComboBox();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.dgPedidos = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgPedidos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(315, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "REPORTE DE PEDIDOS POR CLIENTE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NOMBRE DEL CLIENTE";
+            // 
+            // comboClientes
+            // 
+            this.comboClientes.FormattingEnabled = true;
+            this.comboClientes.Location = new System.Drawing.Point(241, 76);
+            this.comboClientes.Name = "comboClientes";
+            this.comboClientes.Size = new System.Drawing.Size(339, 21);
+            this.comboClientes.TabIndex = 2;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(625, 68);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(88, 29);
+            this.btnConsultar.TabIndex = 3;
+            this.btnConsultar.Text = "CONSULTAR";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.BtnConsultar_Click);
+            // 
+            // dgPedidos
+            // 
+            this.dgPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgPedidos.Location = new System.Drawing.Point(89, 155);
+            this.dgPedidos.Name = "dgPedidos";
+            this.dgPedidos.Size = new System.Drawing.Size(624, 187);
+            this.dgPedidos.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(521, 371);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "TOTAL DE PEDIDOS";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.Location = new System.Drawing.Point(658, 364);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(55, 20);
+            this.lblCantidad.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dgPedidos);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.comboClientes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgPedidos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboClientes;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.DataGridView dgPedidos;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox lblCantidad;
+    }
+}
+
---
+++ Semana01_CP02/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows.Forms;
+
+namespace Semana01_CP02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString);
+        public void ListaClientes()
+        {
+            using(SqlDataAdapter Df = new SqlDataAdapter("Usp_ListadoCompañias_Neptuno",cn))
+            {
+                Df.SelectCommand.CommandType = CommandType.StoredProcedure;
+                using (DataSet Da = new DataSet())
+                {
+                    Df.Fill(Da, "Clientes");
+                    comboClientes.DataSource = Da.Tables["Clientes"];
+                    comboClientes.DisplayMember = "Cliente";
+                }
+            }
+        }
+        public void ListaPedidos()
+        {
+            string selected = comboClientes.GetItemText(comboClientes.SelectedItem);
+            using (SqlDataAdapter Df = new SqlDataAdapter("Usp_ListadoPedidosPorNombre_Neptuno", cn))
+            {
+                Df.SelectCommand.CommandType = CommandType.StoredProcedure;
+                Df.SelectCommand.Parameters.AddWithValue("@Nombre", selected);
+                using (DataSet Da = new DataSet())
+                {
+                    Df.Fill(Da, "Pedidos");
+                    dgPedidos.DataSource = Da.Tables["Pedidos"];
+                    lblCantidad.Text = Da.Tables["Pedidos"].Rows.Count.ToString();
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListaClientes();
+        }
+        private void BtnConsultar_Click(object sender, EventArgs e)
+        {
+            ListaPedidos();
+        }
+    }
+}
